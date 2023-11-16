397767c5f3829e912c85ecd1d08085e1ad37192e
rosi-zh <rosi.zheliazkowa@gmail.com>
2023-11-16 16:32:26 +0200

    Add Game List component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
 import { Route, Routes } from "react-router-dom"
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
+import GameList from "./components/game-list/GameList"
 
 function App() {
 
-  return (
-    <div id="box">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-    </div>
-  )
+    return (
+        <div id="box">
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/games" element={<GameList />} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App
