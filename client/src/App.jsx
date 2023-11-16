9894583baad8187931c9d70977a0201dd71acbfd
rosi-zh <rosi.zheliazkowa@gmail.com>
2023-11-16 16:07:40 +0200

    Add Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/header/Header"
+
 function App() {
 
   return (
-    <>
-        <h2>Hello</h2>
-    </>
+    <div id="box">
+        <Header />
+    </div>
   )
 }
 
