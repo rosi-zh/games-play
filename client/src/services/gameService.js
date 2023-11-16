a4aca3c5ce80c76c6d0c125d56dc646dca8de2f9
rosi-zh <rosi.zheliazkowa@gmail.com>
2023-11-16 20:16:21 +0200

    Show dynamic game list

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,4 +1,12 @@
+import { request } from "../lib/request";
+
 const baseUrl = 'http://localhost:3030/jsonstore/games';
+
+export const getAll = async () => {
+    const result = await request('GET', baseUrl);
+
+    return Object.values(result);
+}
 
 export const create = async (gameData) => {
     const response = await fetch(baseUrl, {
