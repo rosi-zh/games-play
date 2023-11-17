bb1cbd206c002e76515e1cb0617db6e2cf429a58
rosi-zh <rosi.zheliazkowa@gmail.com>
2023-11-17 10:52:03 +0200

    Update requester

--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -10,8 +10,6 @@ export default function GameList() {
             .then(result => setGames(result));
     }, []);
 
-    console.log(games);
-
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
