933601ec32612820d8a4340a5cdb9af078e9e585
rosi-zh <rosi.zheliazkowa@gmail.com>
2023-11-25 16:53:45 +0200

    Add user register

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -10,3 +10,8 @@ export const login = async (email, password) => {
 
     return result;
 }
+
+export const register = (email, password) => authService.post(`${baseUrl}/register`, {
+    email, 
+    password
+});
