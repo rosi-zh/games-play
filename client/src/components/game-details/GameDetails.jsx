a2484fcb42d3ba744d653470513f4abe64f78290
rosi-zh <rosi.zheliazkowa@gmail.com>
2023-11-25 18:18:46 +0200

    Add comments without input field username

--- client/src/components/game-details/GameDetails.jsx
+++ client/src/components/game-details/GameDetails.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import * as gameService from '../../services/gameService';
 import * as commentService from '../../services/commentService';
+import AuthContext from "../../contexts/authContext";
 
 export default function GameDetails() {
+    const { email } = useContext(AuthContext);
     const { gameId } = useParams();
     const [game, setGame] = useState({});
     const [comments, setComments] = useState([]);
@@ -24,13 +26,10 @@ export default function GameDetails() {
 
         const newComment = await commentService.create(
             gameId,
-            formData.get('username'),
             formData.get('comment')    
         )
 
-        setComments(state => [...state, newComment]);
-
-        console.log(newComment);
+        setComments(state => [...state, {...newComment, owner: { email }}]);
     }
 
     return (
@@ -50,9 +49,9 @@ export default function GameDetails() {
                 <div className="details-comments">
                     <h2>Comments:</h2>
                     <ul>
-                        {comments.map(({ _id ,username, text}) => (
+                        {comments.map(({ _id, text, owner: { email } }) => (
                             <li key={_id} className="comment">
-                                <p>{username}: {text}</p>
+                                <p>{email}: {text}</p>
                             </li>
                         ))}
                         
@@ -74,7 +73,6 @@ export default function GameDetails() {
             <article className="create-comment">
                 <label>Add new comment:</label>
                 <form className="form" onSubmit={addCommentHandler}>
-                    <input type="text" name="username" placeholder="Username" />
                     <textarea name="comment" placeholder="Comment......"></textarea>
                     <input className="btn submit" type="submit" value="Add Comment" />
                 </form>
